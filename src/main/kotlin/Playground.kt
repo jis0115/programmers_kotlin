2245e79f2ef2891033854c9ff5fb613990c490fa
Lonepine <lonepin2@gmail.com>
2022-04-29 10:52:20 +0900

    로또의 최고 순위와 최저 순위

--- src/main/kotlin/Playground.kt
+++ src/main/kotlin/Playground.kt
@@ -1,7 +1,12 @@
 
 fun main() {
-    println(solutionReport(arrayOf("muzi", "frodo", "apeach", "neo"), arrayOf("muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"),2).map { it })
-    println(solutionReport(arrayOf("con", "ryan"), arrayOf("ryan con", "ryan con", "ryan con", "ryan con"),3).map { it })
+    //신고 결과 받기
+//    println(solutionReport(arrayOf("muzi", "frodo", "apeach", "neo"), arrayOf("muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"),2).map { it })
+//    println(solutionReport(arrayOf("con", "ryan"), arrayOf("ryan con", "ryan con", "ryan con", "ryan con"),3).map { it })
+
+    //로또의 최고 순위와 최저 순위
+    println(solutionLottoRank(intArrayOf(45, 4, 35, 20, 3, 9), intArrayOf(20, 9, 3, 45, 4, 35)).map { it })
+    println(solutionLottoRank(intArrayOf(0, 0, 0, 0, 0, 0), intArrayOf(38, 19, 20, 40, 15, 25)).map { it })
 }
 
 // 신고 결과 받기
@@ -26,4 +31,11 @@ fun solutionReport(id_list: Array<String>, report: Array<String>, k: Int): IntAr
         }
     }
     return check.values.toIntArray()
+}
+
+//로또의 최고 순위와 최저 순위
+fun solutionLottoRank(lottos: IntArray, win_nums: IntArray): IntArray {
+    fun getWinRank(num:Int) = (6-num) + if (num > 0) {1} else {0}
+    val sameCount = lottos.filter { it>0 }.count {num-> win_nums.contains(num) }
+    return intArrayOf(getWinRank(sameCount+(lottos.count { it == 0 })),getWinRank(sameCount))
 }
